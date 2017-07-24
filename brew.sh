2bc39b76a14d83f65655bf6b0001c36859ea1751
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-07-24 22:05:11 +0200

    Add homebrew to install script and a script for installing homebrew stuff

---
+++ brew.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update everything
+brew update
+brew upgrade
+
+# install the usual suspects
+brew install bash
+brew install bash-completion
+echo "  you'll need to add /usr/local/bin/bash to /etc/shells and then run chsh"
+
+brew install vim --with-override-system-vi
+brew install git
+brew install tmux
--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@
 
 # install vim plug
 curl -fLo  ~/.dotfiles/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install homebrew if missing
+if test ! $(which brew)
+then
+  echo "  installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
