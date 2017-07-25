69ce1b46dee4c82fa8c6df47a2534017cb831df6
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-07-25 08:22:28 +0200

    Add bash-git-prompt

--- brew.sh
+++ brew.sh
@@ -8,6 +8,7 @@ brew upgrade
 brew install bash
 brew install bash-completion
 echo "  you'll need to add /usr/local/bin/bash to /etc/shells and then run chsh"
+brew install bash-git-prompt
 
 brew install vim --with-override-system-vi
 brew install git
