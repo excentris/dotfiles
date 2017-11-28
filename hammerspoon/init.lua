871ec89c16e8c67a99dfa9860cef7ecbd3c12fad
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-11-28 16:25:19 +0100

    Improve currently playing info to include sonos when available

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -17,11 +17,36 @@ local config = hs.json.decode(util.read_file("config.json"));
 
 combo = config.combo
 maker_channel_id = config.maker_channel_id
+sonos_api_url = config.sonos_api_url
 
 -- Display information about music currently playing through Spotify
 hs.hotkey.bind(combo, "q", function()
-  hs.alert.show("Playing: " .. hs.spotify.getCurrentTrack() .. " by " .. hs.spotify.getCurrentArtist(), 3)
+    local spotify = ""
+
+    if hs.spotify.isPlaying() then
+        spotify = spotify .. "This machine playing: " .. hs.spotify.getCurrentTrack() .. " by " .. hs.spotify.getCurrentArtist() .. "\n"
+    end
+
+    status, data, headers = hs.http.get(sonos_api_url .. "/zones", nil)
+
+    local json_data = hs.json.decode(data)
+
+    local sonos = ""
+    if status == 200 then
+        for k, zone in pairs(json_data) do 
+            if zone.coordinator.state.playbackState == "PLAYING" then
+                sonos = sonos .. zone.coordinator.roomName .. " playing: " .. zone.coordinator.state.currentTrack.title .. " by " .. zone.coordinator.state.currentTrack.artist
+                if next(json_data, k) ~= nil then
+                    sonos = sonos .. "\n"  
+                end
+            end
+        end
+    end
+
+    hs.alert.show(spotify .. sonos, 3)
 end)
+
+
 
 -- Save currently playing song to My Library on Spotify
 hs.hotkey.bind(combo, 'k', function ()
