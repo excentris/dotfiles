8c11dc56dcf1095031355e2f10523f5b59d1417a
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-12-02 17:39:39 +0100

    Add tmux session menu

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Let's do this..."
 curl -fLo ~/.dotfiles/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install homebrew if missing
-if test ! $(which brew)
+if test ! $(which brew) 
 then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -23,11 +23,15 @@ xcode-select --install
 # Hammerspoon
 echo "Setting up Hammerspoon..."
 # symlink hammerspoon config
-ln -s ~/.dotfiles/hammerspoon $HOME/.hammerspoon
+ln -sfn $DOTFILES/hammerspoon $HOME/.hammerspoon
 # get hammerspoon config
 cp "$HOME/Google Drive/Config/hammerspoon-config.json" $HOME/.hammerspoon/config.json
 
 # Karabiner-Elements
 echo "Setting up Karabiner-Elements..."
 # symlink karabiner config
-ln -s ~/.dotfiles/karabiner $HOME/.config/karabiner
+ln -sfn $DOTFILES/karabiner $HOME/.config/karabiner
+
+# make scripts executable
+echo "Making scripts executable..."
+chmod u+x $SCRIPTS/*
